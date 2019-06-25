ed9a0f815701ae00a6952c342f35856572401e6c
KoyamaHiroto <31232939+KoyamaHiroto@users.noreply.github.com>
2019-06-25 12:56:05 +0900

    Add files via upload

---
+++ kadai8/src/ByoukiState.java
@@ -0,0 +1,31 @@
+
+/**
+ * ByoukiState
+ */
+public class ByoukiState extends DogState{
+
+    private static ByoukiState s = new ByoukiState();
+
+    private ByoukiState(){
+
+    }
+
+    public static DogState getInstance(){
+        return s;
+    }
+    
+    @Override
+    public void tukareta(Dog moto) {
+
+    }
+
+    @Override
+    public void tabeta(Dog moto) {
+        moto.changeState(FutsuuState.getInstance());
+    }
+
+    public String toString(){
+        return "病気状態";
+    }
+    
+}
